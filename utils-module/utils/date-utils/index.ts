9af1f730175803259d5459ad81b598972de4c721
RoamerSoft <info@roamersoft.com>
2023-08-10 12:10:50 +0100

    Added utc to gmt converter.

--- utils-module/utils/date-utils/index.ts
+++ utils-module/utils/date-utils/index.ts
@@ -1,11 +1,17 @@
 const dateToString = (date: Date): string => {
-	// Make sure the date is a Date object
-	date = new Date(date);
-	const day = date.getDate().toString();
-	const month = (date.getMonth() + 1).toString();
-	const year = date.getFullYear().toString();
+    // Make sure the date is a Date object
+    date = new Date(date.toISOString());
+    const day = date.getDate().toString();
+    const month = (date.getMonth() + 1).toString();
+    const year = date.getFullYear().toString();
 
-	return `${day.length < 2 ? '0' + day : day}-${month.length < 2 ? '0' + month : month}-${year}`;
+    return `${day.length < 2 ? '0' + day : day}-${month.length < 2 ? '0' + month : month}-${year}`;
 };
 
-export { dateToString };
+const utcToGmtPlus2 = (date: Date): Date => {
+    const gmtPlus2Timestamp = new Date(date.toISOString());
+    gmtPlus2Timestamp.setHours(gmtPlus2Timestamp.getHours() + 2);
+    return gmtPlus2Timestamp;
+}
+
+export { dateToString, utcToGmtPlus2 };
