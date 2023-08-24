aa12694b670d646f2695bda9247d61da15e0b700
RoamerSoft <info@roamersoft.com>
2023-08-24 16:03:07 +0100

    Improved sort to work with strings.

--- utils-module/utils/sort-utils/index.ts
+++ utils-module/utils/sort-utils/index.ts
@@ -1,23 +1,31 @@
 enum SortDirection {
-	ASC = 'ASCENDING',
-	DESC = 'DESCENDING'
+    ASC = 'ASCENDING',
+    DESC = 'DESCENDING'
 }
 
 const sortObjects = (objectArray: any[], propertyName: string, sortDirection: SortDirection = SortDirection.ASC): any[] => {
-	// Clone the original array
-	const sortedArray = [...objectArray];
+    // Clone the original array
+    const sortedArray = [...objectArray];
 
-	// Sort based on sort direction
-	switch (sortDirection) {
-		case SortDirection.ASC:
-			sortedArray.sort((a, b) => a[propertyName] - b[propertyName]);
-			break;
-		case SortDirection.DESC:
-			sortedArray.sort((a, b) => b[propertyName] - a[propertyName]);
-			break;
-	}
+// Sort based on sort direction
+    switch (sortDirection) {
+        case SortDirection.ASC:
+            sortedArray.sort((a, b) => {
+                if (a[propertyName] < b[propertyName]) return -1;
+                if (a[propertyName] > b[propertyName]) return 1;
+                return 0;
+            });
+            break;
+        case SortDirection.DESC:
+            sortedArray.sort((a, b) => {
+                if (a[propertyName] > b[propertyName]) return -1;
+                if (a[propertyName] < b[propertyName]) return 1;
+                return 0;
+            });
+            break;
+    }
 
-	return sortedArray;
+    return sortedArray;
 };
 
 export { sortObjects, SortDirection };
